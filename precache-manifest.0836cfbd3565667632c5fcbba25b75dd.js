abbe9f465f644ebec5b4004a48421a0e46623b1e
thangtb172 <thangtb.2113@gmail.com>
2020-06-04 22:35:50 +0700

    Updates

---
+++ precache-manifest.0836cfbd3565667632c5fcbba25b75dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd84870bd661c4ff1312f7d750b510e4",
+    "url": "/easylistening/index.html"
+  },
+  {
+    "revision": "8fb461d1065e18cfc275",
+    "url": "/easylistening/static/css/main.b6335e05.chunk.css"
+  },
+  {
+    "revision": "5406bc0ad47959c1e728",
+    "url": "/easylistening/static/js/2.07b5dcc5.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/easylistening/static/js/2.07b5dcc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fb461d1065e18cfc275",
+    "url": "/easylistening/static/js/main.11058a24.chunk.js"
+  },
+  {
+    "revision": "2cee774668ea220e92e0",
+    "url": "/easylistening/static/js/runtime-main.daf9c159.js"
+  }
+]);
